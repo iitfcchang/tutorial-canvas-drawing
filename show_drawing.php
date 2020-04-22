db917cdffa5fbfc9afe48ad7df2802327d3a39d0
Feng-Cheng Chang <fc.chang@gms.tku.edu.tw>
2020-04-22 12:10:04 +0800

    Add show_drawing.php for showing a single drawing.

---
+++ show_drawing.php
@@ -0,0 +1,25 @@
+<?php
+if (isset($_GET['id'])) {
+    $imgid = $_GET['id'];
+    $bname = get_imgname($imgid);
+    if ($bname!=null) {
+        header('Content-Type', 'image/jpeg');
+        readfile('imgs/'.$bname);
+    } else {
+        die('No File');
+    }
+} else {
+    die('No Content');
+}
+function get_imgname($imgid) {
+    include('/usr/local/etc/drawingsdb_config.php');
+    $dbh = new PDO($dbdsn, $dbuser, $dbpass);
+    $sql = "select name from drawings where id='$imgid';";
+    $rs = $dbh->query($sql)->fetchAll();
+    if (count($rs)==1) {
+        return $rs[0]['name'];
+    } else {
+        return null;
+    }
+}
+?>
